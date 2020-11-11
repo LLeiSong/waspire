26670f502e617ec9c541c7ba0e6f544248191259
LLeiSong <pinot.sung@gmail.com>
2020-11-10 23:12:59 -0500

    Modify path option:art:

--- run_wasp.sh
+++ run_wasp.sh
@@ -16,8 +16,14 @@ shift
 
 # Set directories
 INDIR=/mnt/input-dir
-WORKDIR=/work
+WORKDIR=/work/wasp
 OUTDIR=/mnt/output-dir
+
+# Ensure that workdir is clean
+if [ -d $WORKDIR ]; then
+    rm -rf $WORKDIR
+fi
+mkdir -p $WORKDIR
 
 # Symlink datasets into workdir
 for f in $INDIR/*; do
@@ -27,11 +33,11 @@ done
 # Generate INPUT parameter for WASP
 INPUT=""
 for f in $INDIR/*; do
-    INPUT="${INPUT} ./$(basename $f)/$(basename $f)_MTD_ALL.xml"
+    INPUT="${INPUT} ./wasp/$(basename $f)/$(basename $f)_MTD_ALL.xml"
 done
 
 # Run wasp
-mkdir $WORKDIR/out
+mkdir -p $WORKDIR/out
 /usr/wasp/bin/WASP --input $INPUT --out $WORKDIR/out --date $DATE --synthalf $SYNTHALF
 
 # Copy outputs from workdir
@@ -40,4 +46,5 @@ for f in $WORKDIR/out/*; do
     cp -r $f $OUTDIR
 done
 
+# Remove all files
 rm -rf $WORKDIR
